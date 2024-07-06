03afe63bc3b6913ac4ba0f28e1b6c359f8121b9f
yunnnnchieh <120265058+yunnnnchieh@users.noreply.github.com>
2024-07-06 14:55:56 +0800

    please

--- app/temtem.py
+++ app/temtem.py
@@ -1,22 +1,22 @@
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, JoinEvent, LeaveEvent,TemplateSendMessage,ButtonsTemplate,MessageAction,PostbackAction,ImageCarouselTemplate,ImageCarouselColumn,ImageSendMessage
-
-if msg == '!輪播樣板':
-        carousel_template = CarouselTemplate(columns=[
-            CarouselColumn(
-                text='選項 1',
-                title='標題 1',
-                thumbnail_image_url='https://storage.googleapis.com/你的圖片連結.png',
-                actions=[
-                    MessageAction(label='按鈕 1', text='按鈕 1')
-                ]
-            ),
-            CarouselColumn(
-                text='連結',
-                title='連結',
-                thumbnail_image_url='https://storage.googleapis.com/你的圖片連結.png',
-                actions=[
-                    URIAction(label='前往GOOGLE', uri='https://www.google.com')
-                ]
-            )
-        ])
-        return template_message(alt_text='輪播樣板', template=carousel_template)
+def template_message(msg):
+    if msg == '!輪播樣板':
+            carousel_template = CarouselTemplate(columns=[
+                CarouselColumn(
+                    text='選項 1',
+                    title='標題 1',
+                    thumbnail_image_url='../img/大吉.png',
+                    actions=[
+                        MessageAction(label='按鈕 1', text='按鈕 1')
+                    ]
+                ),
+                CarouselColumn(
+                    text='連結',
+                    title='連結',
+                    thumbnail_image_url='../img/大吉.png',
+                    actions=[
+                        URIAction(label='前往GOOGLE', uri='https://www.google.com')
+                    ]
+                )
+            ])
+            return (alt_text='輪播樣板', template=carousel_template)
