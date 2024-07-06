3af6018e9d387085ba5eb513437a098ae5f3b86a
eating <eva930514@gmail.com>
2024-07-07 01:40:31 +0800

    fix news

--- app/line_bot.py
+++ app/line_bot.py
@@ -269,6 +269,7 @@ def handle_message(event):
             columns = []
             for item in news_items:
                 column = CarouselColumn(
+                    thumbnail_image_url=item['image'] if item.get('image') else 'https://storage.googleapis.com/sitconimg/img/news.png',
                     text=item['title'][:35] + '...' if len(item['title']) > 40 else item['title'],
                     actions=[
                         URIAction(
