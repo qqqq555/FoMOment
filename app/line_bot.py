d543f53c4b0b6bd793dfcad0b32bfa1408afae67
v1c999 <v1ct0riiaach0u@gmail.com>
2024-07-07 11:10:12 +0800

    please

--- app/line_bot.py
+++ app/line_bot.py
@@ -111,41 +111,39 @@ def handle_message(event):
                         '最低價': lines[7].split(': ')[1],
                         '昨收價': lines[8].split(': ')[1],
                         '更新時間': lines[9].split(': ')[1]
-                    }
+                     }
 
                     columns = []
                     column = CarouselColumn(
-                        text=f"公司簡稱: {stock_data['公司簡稱']}\n"
-                            f"成交價: {stock_data['成交價']}\n"
-                            f"漲跌百分比: {stock_data['漲跌百分比']}\n"
-                            f"成交量: {stock_data['成交量']}\n"
-                            f"開盤價: {stock_data['開盤價']}\n"
-                            f"最高價: {stock_data['最高價']}\n"
-                            f"最低價: {stock_data['最低價']}\n"
-                            f"昨收價: {stock_data['昨收價']}\n"
-                            f"更新時間: {stock_data['更新時間']}\n",
-                        actions=[
-                            URIAction(
-                                label='查看詳細資訊',
-                                uri=f'https://tw.search.yahoo.com/search?p={stock_code}&fr=finance&fr2=p%3Afinvsrp%2Cm%3Asb'
-                            )
-                        ]
-                    )
+                         text=f"公司簡稱: {stock_data['公司簡稱']}\n"
+                             f"成交價: {stock_data['成交價']}\n"
+                             f"漲跌百分比: {stock_data['漲跌百分比']}\n"
+                             f"成交量: {stock_data['成交量']}\n"
+                             f"開盤價: {stock_data['開盤價']}\n"
+                             f"最高價: {stock_data['最高價']}\n"
+                             f"最低價: {stock_data['最低價']}\n"
+                             f"昨收價: {stock_data['昨收價']}\n"
+                             f"更新時間: {stock_data['更新時間']}\n",
+                         actions=[
+                             URIAction(
+                                 label='查看詳細資訊',
+                                 uri=f'https://tw.search.yahoo.com/search?p={stock_code}&fr=finance&fr2=p%3Afinvsrp%2Cm%3Asb'
+                             )
+                         ]
+                     )
                     columns.append(column)
-
+                    
                     carousel_template = CarouselTemplate(columns=columns)
                     template_message = TemplateSendMessage(
                         alt_text='股票資訊',
                         template=carousel_template
                     )
-
                     line_bot_api.reply_message(event.reply_token, template_message)
-
             except Exception as e:
                 line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text="抱歉，處理股票資訊時發生錯誤。")
-                )
+                     TextSendMessage(text="抱歉，處理股票資訊時發生錯誤。")
+                 )
             return
 
         elif user_message == '拜託':
